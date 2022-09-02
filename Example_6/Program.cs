93696c337a95a0ca5d40ca19c633242e6cbb32a8
Irina <Aniri_Varga@mail.ru>
2022-09-02 21:36:41 +0300

    added example 6 kettlebells

---
+++ Example_6/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 5;
+int b = 9;
+int c = 7;
+int d = 2;
+int e = 4;
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
