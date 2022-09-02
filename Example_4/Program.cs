8320aa314a9b1571ca7503049e1b52ee883328bb
Irina <Aniri_Varga@mail.ru>
2022-09-02 20:53:31 +0300

    Added example 4 random sum

--- Example_4/Program.cs
+++ Example_4/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 14;
-double numberB = 5;
+﻿double numberA = new Random().Next(1,5);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,7);
+Console.WriteLine(numberB);
 double result = numberA / numberB;
 Console.WriteLine(result);
