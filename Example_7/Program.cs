4633566d1503e2274eef8ade3dcd7628188973eb
Irina <Aniri_Varga@mail.ru>
2022-09-02 22:06:02 +0300

    added example 7 triangles

---
+++ Example_7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.SetCursorPosition(1, 10);
+Console.WriteLine("*");
+int xa = 1, ya = 1,
+    xb = 1, yb = 20,
+    xc = 40, yc = 20;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("*");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("*");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("*");
