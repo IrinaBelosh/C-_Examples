6f28638597e7e62a346ecc70b7c365aa42831e27
Irina <Aniri_Varga@mail.ru>
2022-09-02 21:20:18 +0300

    Added example 5 Hi Mary

---
+++ Example_5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console. WriteLine("Enter your name please: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "mary")
+{
+    Console.WriteLine("Hi, my dear mary!");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
