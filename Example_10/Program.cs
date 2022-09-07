311078973f41cb34aa5f8a77e47c2c8842859857
Irina <Aniri_Varga@mail.ru>
2022-09-07 22:09:50 +0300

    added example 10

---
+++ Example_10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти первый индекс числа find в массиве.
+Console.Clear();
+int[] array = { 1, 75, 3, 16, 2, 6, 11, 3, 9 };
+int size = array.Length;
+int find = 4;
+int index = 0;
+if (index < size)
+{
+    while (index < size)
+    {
+        if (array[index] == find)
+        {
+            Console.WriteLine(index);
+        }
+        index++;
+    }
+}
+else
+{
+    Console.WriteLine("no match");
+}
+
